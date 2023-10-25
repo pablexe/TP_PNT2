be109b8887accce6b5d943c0a6d6b2f429dc7c4f
Pablo Estrada <pabloestrada1983@gmail.com>
2023-10-24 22:54:31 -0300

    login view

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,11 @@
 <script>
-import {IonPage, IonContent,IonInput,IonButton} from '@ionic/vue'
+import { IonPage, IonContent,IonInput,IonButton,IonLabel, IonItem, IonAlert} from '@ionic/vue'
 import { storeToRefs } from 'pinia';
 import { loginStore } from "../stores/userStore"
 import router from '../router';
+
 export default {
-  components: {IonPage,IonContent,IonInput,IonButton},
+  components: {IonPage,IonContent,IonInput,IonButton,IonLabel,IonItem,IonAlert},
   setup(){
     const store = loginStore;
     const {estaLogeado}  =storeToRefs(store);
@@ -13,19 +14,27 @@ export default {
   },
   data(){
     return {
-        user:{}
+        user:{},
+        errorMesasge:''
     }
   },
   methods:{
     async loginForm(){
-        console.log(this.user.email)
+      try{
         await this.login(this.user);
         if(this.estaLogeado){
             this.$router.push("/system")
         }else {
             router.push("/system")
         }
-    }
+      }catch (error){
+        this.errorMesasge ='Erro al iniciar sesion. Por favor intente nuevamente';
+      }
+    },
+    RegisterForm(){
+      this.$router.push('/register')
+  }
+
   }
 }
 </script>
@@ -33,14 +42,66 @@ export default {
 <template>
   <ion-page>
     <ion-content>
-        <h2>Login</h2>
-        <ion-input v-model="user.email" label="email" type="email"></ion-input>
-        <ion-input v-model="user.password" label="password" type="password"></ion-input>
-        <ion-button @click="loginForm">Login</ion-button>
+      <div class="content-wrapper">
+      <div class="login-container">
+        <img src="@/assets/logo2.png" alt="Logo" class="login-logo">
+        <h5>Login</h5>
+        <ion-item>
+          <ion-label position="floating">Email</ion-label>
+          <ion-input v-model="user.email" type="email"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Password</ion-label>
+          <ion-input v-model="user.password" type="password"></ion-input>
+        </ion-item>
+        <ion-button expand="full" @click="loginForm">Login</ion-button>
+        <p>No estás Registrado? <a @click="RegisterForm">Registrate!</a></p>
+        <ion-alert
+           :is-open="!!errorMessage"
+            @did-dismiss="() => errorMessage = ''"
+            header="Error"
+            :message="errorMessage"
+            :buttons="['OK']"
+        ></ion-alert>
+      </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <style>
+
+
+.content-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center ;
+  height: 100%;
+  width: 100%;
+}
+
+.login-container{
+  transform: translateY(-100px);
+  max-width: 400px;
+  width: 100%;
+  margin:auto;
+  text-align: center;
+}
+.login-logo{
+  width: 450px;
+  margin: 0 auto 20px;
+  display: block;
+}
+
+a {
+  color: #007bff; 
+  text-decoration: none;
+  cursor: pointer;
+}
+
+a:hover {
+  text-decoration: underline; 
+}
+
 </style>
 

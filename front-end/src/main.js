3121acdd70a8882cffd472409da585ea1256a78a
Pablo Estrada <pabloestrada1983@gmail.com>
2023-11-08 21:29:12 -0300

    Creacion de views Home u Productos

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,14 +1,18 @@
 import './assets/main.css'
 
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import {IonicVue} from '@ionic/vue'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
-
+app.use(BootstrapVue)
+app.use(IconsPlugin)
 app.use(createPinia())
+app.use(VueAxios,axios)
 app.use(router)
 
 app.mount('#app')
